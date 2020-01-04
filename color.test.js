60576811f4b8ac298c9f4616fd3420c29d891e01
ZoneTwelve <zonetwelve159@gmail.com>
2020-01-04 16:14:58 +0800

    [add] package.json

--- color.test.js
+++
@@ -1,6 +0,0 @@
-const color = require("./modules/terminal-color");
-
-color.print("white", "black", "Hello");
-process.stdout.write(" ");
-//color.print("blue", "red", " ");
-color.println("green", "red", "World");
